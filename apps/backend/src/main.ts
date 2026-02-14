767271f517b629e71727f4f0b9e09cb9e5cc47f6
@akshit.jain02 <akshit.jain02@nagarro.com>
2026-02-14 18:13:59 +0530

    fix: allow production frontend origin in cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -8,7 +8,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    // origin: 'http://localhost:5173',
+    origin: true,
+    credentials: true,
   });
 
   const server = app.getHttpAdapter().getInstance();
