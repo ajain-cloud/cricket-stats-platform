034e522a809caa8442b6583f22d23909febab52e
@akshit.jain02 <akshit.jain02@nagarro.com>
2026-02-21 18:34:50 +0530

    fix. Update backend url in FE

--- apps/frontend/src/api/axios.ts
+++ apps/frontend/src/api/axios.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const axiosApi = axios.create({
   // baseURL: 'http://localhost:3000',
-  baseURL: 'http://64.227.173.163:3000',
+  baseURL: 'http://134.209.158.177:3000',
 });
